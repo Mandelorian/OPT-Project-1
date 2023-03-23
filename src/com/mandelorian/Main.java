6b0db77b7140cd2a249b23064013e72a3c4ed105
Chris Yeung <jackknife29jk@gmail.com>
2023-03-23 14:34:32 +0100

    commit chris void start.

--- src/com/mandelorian/Main.java
+++ src/com/mandelorian/Main.java
@@ -4,7 +4,9 @@ import com.mandelorian.boat.Boat;
 import com.mandelorian.quotation.Quotation;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
@@ -41,9 +43,51 @@ class Program {
         this.currentQuotation = quotation;
     }
 
+    public void clearScreen() {
+        for (int i = 0; i < 200; i++) {System.out.println();}
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
+
 
     public void start() {
         System.out.println("Starting program.");
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welkom!");
+        System.out.println("Welk type boot wilt u kopen?");
+        System.out.println();
+
+        System.out.println("----------------------------------------");
+        System.out.println("               Boot lijst               ");
+        System.out.println("----------------------------------------");
+
+        Boat boat = null;
+        for(int i = 0; i < Memory.getBoatList().size(); i++) {
+            System.out.println((i + 1) + ". " + Memory.getBoatList().get(i).getName());
+        }
+
+        System.out.println("----------------------------------------");
+
+        System.out.println();
+
+        while (boat == null) {
+            System.out.print("Kies de bootnummer van de boot die je wil: ");
+            int boatNumber = scanner.nextInt();
+            System.out.println();
+
+            if(boatNumber >= Memory.getBoatList().size() || Memory.getBoatList().get((boatNumber - 1)) == null) {
+                System.out.println("De boot met dit nummer bestaat niet.");
+                continue;
+            }
+
+            boat = Memory.getBoatList().get((boatNumber - 1));
+        }
+
+        this.clearScreen();
+
+        Quotation quotation = this.createNewQuotation();
+        this.setCurrentQuotation(quotation);
     }
 }
--- src/com/mandelorian/boat/Option.java
+++ src/com/mandelorian/boat/Option.java
@@ -2,18 +2,13 @@ package com.mandelorian.boat;
 
 public class Option extends Item {
     private String description;
-    private Boat boat;
 
-
-    public Option(String name, String description, Boat boat) {
+    public Option(String name, String description, Boat boatByName) {
         super(name);
         this.description = description;
-        this.boat = boat;
     }
 
     public String getDescription() {return this.description;}
 
     public void setDescription(String description) {this.description = description;}
-
-    public Boat getBoat() {return boat;}
 }
