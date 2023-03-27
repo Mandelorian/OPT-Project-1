2c03b66ee66a54d4a1955b02795a02d16311ee27
David <david.out@migrato.nl>
2023-03-27 09:52:40 +0200

    Revert "Delete Quotation.java"
    This reverts commit efa11fd551be9087e42ffe75c883f01b22eee34f.

---
+++ src/com/mandelorian/quotation/Quotation.java
@@ -0,0 +1,45 @@
+package com.mandelorian.quotation;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.mandelorian.product.Boat;
+import com.mandelorian.product.Option;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quotation {
+
+    // Offerte
+
+    private Boat boat;
+    private List<Option> optionList;
+
+    public Quotation(Boat boat) {
+        this.boat = boat;
+        this.optionList = new ArrayList<>();
+    }
+
+    public Boat getBoat() {return this.boat;}
+    public List<Option> getOptionList() {return this.optionList;}
+
+    public void setBoat(Boat boat) {this.boat = boat;}
+    public void addOption(Option option) {this.optionList.add(option);}
+    public void removeOption(Option option) {this.optionList.remove(option);}
+    public void removeOption(int index) {this.optionList.remove(index);}
+
+    public double getTotalPrice() {
+        double total = ((boat == null) ? 0 : boat.getPrice());
+        System.out.println("boat"+boat.getPrice());
+        for(Option currentOption : optionList) {
+            total += currentOption.getPrice();
+        }
+        System.out.println("total"+total);
+        return  total;
+    }
+
+}
