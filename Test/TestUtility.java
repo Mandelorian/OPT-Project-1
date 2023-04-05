e1d1a66e8f443416ddf3091bd989b789e3e827bf
Jason <22098186@student.hhs.nl>
2023-04-05 14:23:32 +0200

    libarys toegevoegd
    ik heb twee junit libarys toegevoegd en twee tests voor de utility class gemaakt

--- Test/TestUtility.java
+++ Test/TestUtility.java
@@ -1,2 +1,38 @@
+import com.mandelorian.library.Utility;
+import com.mandelorian.product.Boat;
+import com.mandelorian.product.Option;
+import com.mandelorian.product.ProductList;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestUtility {
+
+    @Test
+    public void testGetBoatByName() {
+        //Arrange
+        String testBootNaam = "testBoot";
+        Boat testBoot = new Boat(testBootNaam, 11111111, Utility.getCategorieByName("Grote boten"));
+        ProductList.getBoatList().add(testBoot);
+
+        //Act
+        Boat testBoot2 = Utility.getBoatByName(testBootNaam);
+
+        //Assert
+        assertEquals(testBoot, testBoot2);
+    }
+
+    @Test
+    public void testGetOptionByName() {
+        //Arrange
+        String testOptionNaam = "testOption";
+        Option testOption = new Option(testOptionNaam, 11111111, null, null);
+        ProductList.getOptionList().add(testOption);
+
+        //Act
+        Option testOption2 = Utility.getOptionByName(testOptionNaam);
+
+        //Assert
+        assertEquals(testOption, testOption2);
+    }
 }
